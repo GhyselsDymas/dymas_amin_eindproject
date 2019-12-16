6d920baae648d641c386d80b90e8edfc0283437e
GhyselsDymas <dg.dymasghysels@gmail.com>
2019-12-16 15:46:58 +0100

    Heb een Image link toegevoegt aan class DierenProduct

--- src/main/java/be/ehb/dymas_amin_eindproject/model/DierenProduct/DierenProduct.java
+++ src/main/java/be/ehb/dymas_amin_eindproject/model/DierenProduct/DierenProduct.java
@@ -10,7 +10,7 @@ import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 /**
- * @author Ahmadi Baloutaki Amin
+ * @author Ghysels dymas
  * */
 
 @Entity
--- src/main/java/be/ehb/dymas_amin_eindproject/model/DierenProduct/DierenProductDAO.java
+++ src/main/java/be/ehb/dymas_amin_eindproject/model/DierenProduct/DierenProductDAO.java
@@ -2,5 +2,9 @@ package be.ehb.dymas_amin_eindproject.model.DierenProduct;
 
 import org.springframework.data.repository.CrudRepository;
 
+/**
+ * @author Ghysels dymas
+ * */
+
 public interface DierenProductDAO extends CrudRepository <DierenProduct, Integer> {
 }
