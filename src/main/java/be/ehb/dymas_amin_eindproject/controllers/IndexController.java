3051e7b83c1d49689cd3c0470afbfb7cb6ba4e40
amin.ahmadi.baloutaki@gmail.com <Shahrzad1986>
2019-12-16 15:55:34 +0100

    Added (DierenProduct) IndexControllercontroller

--- src/main/java/be/ehb/dymas_amin_eindproject/controllers/IndexController.java
+++ src/main/java/be/ehb/dymas_amin_eindproject/controllers/IndexController.java
@@ -1,4 +1,31 @@
 package be.ehb.dymas_amin_eindproject.controllers;
 
+import be.ehb.dymas_amin_eindproject.model.DierenProduct.DierenProduct;
+import be.ehb.dymas_amin_eindproject.model.DierenProduct.DierenProductDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @author Ahmadi Baloutaki Amin
+ * */
+@Controller
 public class IndexController {
+
+    @Autowired
+    DierenProductDAO dao;
+
+/*een modelatribute aangemaakt om alle producten te kunnen overlopen met Iterable */
+    @ModelAttribute(value = "allPruducts")
+    public Iterable<DierenProduct>getAllProducts(){
+        return dao.findAll();
+    }
+/*RequestMapping om op de pagina index terecht tekomen*/
+    @RequestMapping(value = {"/index","/"},method = RequestMethod.GET)
+    public String showIndex(ModelMap map){
+        return "index";
+    }
 }
