3946a44b508806d1ace33febc42f783b86dfa9e2
GhyselsDymas <dg.dymasghysels@gmail.com>
2019-12-16 15:40:30 +0100

    HTML and CSS bestanden aangemaakt

--- src/main/java/be/ehb/dymas_amin_eindproject/controllers/IndexController.java
+++ src/main/java/be/ehb/dymas_amin_eindproject/controllers/IndexController.java
@@ -1,4 +1,11 @@
 package be.ehb.dymas_amin_eindproject.controllers;
 
+import be.ehb.dymas_amin_eindproject.model.DierenProduct.DierenProductDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class IndexController {
+    @Autowired
+    DierenProductDAO dao;
 }
--- src/main/java/be/ehb/dymas_amin_eindproject/model/DierenProduct/DierenProduct.java
+++ src/main/java/be/ehb/dymas_amin_eindproject/model/DierenProduct/DierenProduct.java
@@ -9,6 +9,10 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
+/**
+ * @author Ahmadi Baloutaki Amin
+ * */
+
 @Entity
 public class DierenProduct {
 
