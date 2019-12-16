01c8a2fe0ae9066072e500e1d25a6e0f57a4910a
mobapp04 <mobapp04@student.ehb.be>
2019-12-16 14:43:31 +0100

    Initial commit Bestand aangemaakt en klaar voor samenwerking

---
+++ src/main/java/be/ehb/dymas_amin_eindproject/DymasAminEindprojectApplication.java
@@ -0,0 +1,13 @@
+package be.ehb.dymas_amin_eindproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DymasAminEindprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DymasAminEindprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/ehb/dymas_amin_eindproject/DymasAminEindprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ehb.dymas_amin_eindproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DymasAminEindprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
