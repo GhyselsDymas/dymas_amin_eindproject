bb15c74a29b0b4d61d1d99761c9ad64ca8a722fe
GhyselsDymas <dg.dymasghysels@gmail.com>
2019-12-17 10:30:30 +0100

    DierenProductDAO test voor Amin

--- src/main/java/be/ehb/dymas_amin_eindproject/controllers/ProductFilterController.java
+++ src/main/java/be/ehb/dymas_amin_eindproject/controllers/ProductFilterController.java
@@ -10,6 +10,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+/**
+ * @author Ghysels dymas
+ * */
+
 @Controller
 public class ProductFilterController {
 
